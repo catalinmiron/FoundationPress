97bf32748fcf310d9dfab817a05400538d8734f8
catalinmiron <catalin.miron@ubervu.com>
2014-03-11 21:54:08 +0200

    Add more style to next and prev buttons and post views functions

--- functions.php
+++ functions.php
@@ -3,10 +3,8 @@
 Author: Ole Fredrik Lie
 URL: http://olefredrik.com
 */
-
-
 // Various clean up functions
-require_once('library/cleanup.php'); 
+require_once('library/cleanup.php');
 
 // Required for Foundation to work properly
 require_once('library/foundation.php');
@@ -28,5 +26,57 @@ require_once('library/enqueue-scripts.php');
 
 // Add theme support
 require_once('library/theme-support.php');
+
+// function to display number of posts.
+function getPostViews($postID){
+    $count_key = 'post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+        return "0 View";
+    }
+    return $count.' Views';
+}
+
+// function to count views.
+function setPostViews($postID) {
+    $count_key = 'post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+    }else{
+        $count++;
+        update_post_meta($postID, $count_key, $count);
+    }
+}
+
+// Add it to a column in WP-Admin
+add_filter('manage_posts_columns', 'posts_column_views');
+add_action('manage_posts_custom_column', 'posts_custom_column_views',5,2);
+function posts_column_views($defaults){
+    $defaults['post_views'] = __('Views');
+    return $defaults;
+}
+function posts_custom_column_views($column_name, $id){
+  if($column_name === 'post_views'){
+        echo getPostViews(get_the_ID());
+    }
+}
+
+// Prev / Next buttons custom class to a href="" tags
+
+function add_class_next_post_link($html){
+  $html = str_replace('<a','<a class="button small next right"',$html);
+  return $html;
+}
+add_filter('next_post_link','add_class_next_post_link',10,1);
+function add_class_previous_post_link($html){
+  $html = str_replace('<a','<a class="button small prev left"',$html);
+  return $html;
+}
+add_filter('previous_post_link','add_class_previous_post_link',10,1);
 
 ?>
